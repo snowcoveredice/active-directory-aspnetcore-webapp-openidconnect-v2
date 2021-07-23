ecb4ccd0e676eb4365ec835041fa92f74af972b2
Roderik Boerma <rboerma@brandnewday.nl>
2021-07-23 17:26:18 +0200

    Added group administration

---
+++ 5-WebApp-AuthZ/5-2-Groups/Controllers/GroupAssignController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Constants = WebApp_OpenIDConnect_DotNet.Infrastructure.Constants;
+
+namespace WebApp_OpenIDConnect_DotNet.Controllers
+{
+    public class GroupAssignController : Controller
+    {
+        private readonly GraphServiceClient graphServiceClient;
+
+        public GroupAssignController(GraphServiceClient graphServiceClient)
+        {
+            this.graphServiceClient = graphServiceClient;
+        }
+
+        [Authorize(Policy = "GroupAdmin")]
+        [AuthorizeForScopes(Scopes = new[] { Constants.ScopeUserRead })]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            var groupsPage = await graphServiceClient.Groups.Request().GetAsync(cancellationToken);
+            ViewData["Groups"] = groupsPage.ToList();
+            return View();
+        }
+
+        [Authorize(Policy = "GroupAdmin")]
+        [AuthorizeForScopes(Scopes = new[] { Constants.ScopeUserRead })]
+        [HttpGet]
+        [Route("Group")]
+        public async Task<IActionResult> Group(CancellationToken cancellationToken, string groupId)
+        {
+            var group = await graphServiceClient.Groups[groupId].Request().GetAsync(cancellationToken);
+            ViewData["Group"] = group;
+
+            var groupMembersPage = await graphServiceClient.Groups[groupId].Members.Request().GetAsync(cancellationToken);
+            var groupMemberIds = groupMembersPage.Select(member => member.Id).ToList();
+
+            var usersPage = await graphServiceClient.Users.Request().GetAsync(cancellationToken);
+            ViewData["UsersNotInGroup"] = usersPage.Where(user => !groupMemberIds.Contains(user.Id)).ToList();
+            ViewData["UsersInGroup"] = usersPage.Where(user => groupMemberIds.Contains(user.Id)).ToList();
+            return View();
+        }
+
+        [Authorize(Policy = "GroupAdmin")]
+        [AuthorizeForScopes(Scopes = new[] { Constants.ScopeUserRead })]
+        [HttpPost]
+        [Route("Assign")]
+        public async Task<IActionResult> Assign(CancellationToken cancellationToken, string groupId, string[] userIds)
+        {
+            foreach (var userId in userIds)
+            {
+                var userToAdd = await graphServiceClient.Users[userId].Request().GetAsync(cancellationToken);
+                await graphServiceClient.Groups[groupId].Members.References.Request().AddAsync(userToAdd, cancellationToken);
+            }
+
+            var group = await graphServiceClient.Groups[groupId].Request().Expand("members").GetAsync(cancellationToken);
+            ViewData["Group"] = group;
+
+            return View("Members");
+        }
+
+        [Authorize(Policy = "GroupAdmin")]
+        [AuthorizeForScopes(Scopes = new[] { Constants.ScopeUserRead })]
+        [HttpPost]
+        [Route("Remove")]
+        public async Task<IActionResult> Remove(CancellationToken cancellationToken, string groupId, string[] userIds)
+        {
+            foreach (var userId in userIds)
+            {
+                var userToAdd = await graphServiceClient.Users[userId].Request().GetAsync(cancellationToken);
+                await graphServiceClient.Groups[groupId].Members[userId].Reference.Request().DeleteAsync(cancellationToken);
+            }
+
+            var group = await graphServiceClient.Groups[groupId].Request().Expand("members").GetAsync(cancellationToken);
+            ViewData["Group"] = group;
+
+            return View("Members");
+        }
+    }
+}
---
+++ 5-WebApp-AuthZ/5-2-Groups/README_Setting-up-group-assign.md
@@ -0,0 +1,13 @@
+﻿# Setting up GroupAssign
+This project is forked from the official MS example for Group based authorization. It has been expanded with a group administration module.
+
+## User roles
+Make sure any user who will be adding or removing members from groups have sufficient rights on the directory. The role 'Directory writers' grants these rights for instance.
+
+### Application
+Make sure the Active Directory App has sufficient API Permissions. The required permissions are:
+- Directory.AccessAsUser.All
+- Directory.ReadWrite.All
+- Group.ReadWrite.All
+- Groupmember.ReadWrite.All
+- User.ReadWrite.All
